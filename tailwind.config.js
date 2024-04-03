bafcf1856d9402d63962bb64c980be055885992d
vinipolitta <vinicius.politta1@gmail.com>
2024-04-03 07:33:34 -0300

    feat:  created animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,24 @@ module.exports = {
       },
       fontFamily: {
         inter: ['Inter,', 'sans-serif']
+      },
+      keyframes: {
+        sino_kf: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)'
+          },
+          '50%': {
+            transform: 'rotate(10deg)'
+          }
+        }
+      },
+      animation: {
+        sino: 'sino_kf 0.31s ease-in-out infinite'
       }
     },
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
   plugins: [require("daisyui")],
 }
